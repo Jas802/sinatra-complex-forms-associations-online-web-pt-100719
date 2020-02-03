8b0439451f1b3960ec46adcddad23ade92667f97
Jas Spencer <jas.spencer414@gmail.com>
2020-02-03 16:51:39 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,9 +14,7 @@ class PetsController < ApplicationController
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-
     @pet.save
-
     redirect to "pets/#{@pet.id}"
   end
 
@@ -39,7 +37,6 @@ class PetsController < ApplicationController
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-
     redirect to "pets/#{@pet.id}"
   end
 end
